e4fe75f46f249b16a0f10cc70a5a4e851931608c
Martin Drab <martin.drab@email.cz>
2017-03-28 01:42:50 +0200

    More debug macros

--- USBlocker/debug-macros.h
+++ USBlocker/debug-macros.h
@@ -18,14 +18,17 @@
 #define DEBUG_EXIT_FUNCTION_VOID()	\
 	DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_TRACE_LEVEL, "[%u:%u:%u]: " __FILE__ ": " __FUNCTION__ "(-)\n", PsGetCurrentProcessId(), PsGetCurrentThreadId(), KeGetCurrentIrql())
 
+#define DEBUG_MSG(aFormat, ...)	\
+	DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_TRACE_LEVEL, "[%u:%u:%u]: " __FILE__ ": " __FUNCTION__ ": " aFormat "\n", PsGetCurrentProcessId(), PsGetCurrentThreadId(), KeGetCurrentIrql(), __VA_ARGS__)
+
 #else
 
-#define DEBUG_ENTER_FUNCTION(aFormat, ...)
-#define DEBUG_ENTER_FUNCTION_NO_ARGS()
-#define DEBUG_EXIT_FUNCTION(aFormat, ...)
-#define DEBUG_EXIT_FUNCTION_VOID()
-
-
+#define DEBUG_ENTER_FUNCTION(aFormat, ...)		{	}
+#define DEBUG_ENTER_FUNCTION_NO_ARGS()			{	}
+#define DEBUG_EXIT_FUNCTION(aFormat, ...)		{	}
+#define DEBUG_EXIT_FUNCTION_VOID()				{	}
+#define DEBUG_EXIT_FUNCTION_VOID()				{	}
+#define DEBUG_MSG(aFormat, ...)					{	}
 
 #endif
 
