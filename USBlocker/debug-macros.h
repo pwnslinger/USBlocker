c091fb4269657f96ee2ab6ba65fbf7630339978f
mohsen ahmadi <ahmadi@agpp.ir>
2017-04-12 14:54:19 +0430

    modified upper-filter driver to lowe-filter driver. some small modifications and debug strings...

--- USBlocker/debug-macros.h
+++ USBlocker/debug-macros.h
@@ -1,7 +1,5 @@
-
 #ifndef __DEBUG_MACROS_H__
 #define __DEBUG_MACROS_H__
-
 
 
 #define USE_DEBUG_PRINTS
@@ -26,6 +24,19 @@
 	DbgPrintEx(DPFLTR_DEFAULT_ID, DBGM_TRACE_LEVEL, "[%u:%u:%u]: " __FILE__ ": " __FUNCTION__ ": " aFormat "\n", PsGetCurrentProcessId(), PsGetCurrentThreadId(), KeGetCurrentIrql(), __VA_ARGS__)
 
 #else
+
+#define DEBUG_ENTER_FUNCTION(aFormat, ...)		{	}
+#define DEBUG_ENTER_FUNCTION_NO_ARGS()			{	}
+#define DEBUG_EXIT_FUNCTION(aFormat, ...)		{	}
+#define DEBUG_EXIT_FUNCTION_VOID()				{	}
+#define DEBUG_EXIT_FUNCTION_VOID()				{	}
+#define DEBUG_MSG(aFormat, ...)					{	}
+
+#endif
+
+
+
+
 
 
 #endif
